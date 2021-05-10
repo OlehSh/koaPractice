cd5c44735b8232591753a8c87dd869c1cf3c3d97
OLeg <shubinoleh@gmail.com>
2021-05-10 20:46:41 +0300

    remove neo4j from auth

--- src/initStrategy.ts
+++ src/initStrategy.ts
@@ -4,7 +4,7 @@ import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
 import Profile, { ProfileData } from "./service/Profile";
 import env from "./env";
 
-const profileService = container.resolve(Profile)
+// const profileService = container.resolve(Profile)
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: env.secretKey,
@@ -12,16 +12,17 @@ const opts = {
 
 export default function initStrategy(): void {
   koaPassport.use('jwt', new JwtStrategy(opts, (payload: ProfileData, done) => {
-    profileService.fetch(payload.id)
-      .then(user => {
-        if (user) {
-          return done(null, user)
-        } else {
-          return done(null, false)
-        }
-      })
-      .catch(e => {
-        return done(e)
-      })
+    // profileService.fetch(payload.id)
+    //   .then(user => {
+    //     if (user) {
+    //       return done(null, user)
+    //     } else {
+    //       return done(null, false)
+    //     }
+    //   })
+    //   .catch(e => {
+    //     return done(e)
+    //   })
+    done(null, payload)
   }));
 }
