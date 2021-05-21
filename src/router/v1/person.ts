501ee5e0c9f80229b91d37182ba1af5bd5efd71c
OLeg <shubinoleh@gmail.com>
2021-05-21 15:02:16 +0300

    restore db connection, update Dockerfile

--- src/router/v1/person.ts
+++ src/router/v1/person.ts
@@ -4,31 +4,31 @@ import {container} from "tsyringe";
 import { CONTENT_TYPE } from "../../constants/constants";
 import { personCreateBodyValidate, personUpdateBodyValidate} from "../../validate/person";
 import { deleteRelationBodyValidate } from "../../validate/general";
-// import Person from "../../service/Person";
-// import { QueryResult } from "neo4j-driver";
+import Person from "../../service/Person";
+import { QueryResult } from "neo4j-driver";
 import { DeleteRelationBody } from "../interface";
 
 const person = router();
-// const personService = container.resolve(Person)
+const personService = container.resolve(Person)
 person.prefix('/person/')
 
 person.get('/', koaPassport.authenticate('jwt', {session: false }), (ctx) => {
   ctx.body = 'get persons'
 })
 
-person.get('/:id', koaPassport.authenticate('jwt', {session: false}), (ctx) => {
-  // const { id } = ctx.params
-  // ctx.body = await personService.fetch(id)
+person.get('/:id', koaPassport.authenticate('jwt', {session: false}), async (ctx) => {
+  const { id } = ctx.params
+  ctx.body = await personService.fetch(id)
   ctx.body = "success"
 })
 
 person.post('/',
   {validate: {type: CONTENT_TYPE.JSON, body: personCreateBodyValidate}},
   koaPassport.authenticate('jwt', {session: false}),
-  (ctx) => {
+  async (ctx) => {
     try {
-      // const {name, lastName, relation} = ctx.request.body
-      // ctx.body = await personService.add({name, lastName, relation})
+      const { name, lastName, relation } = ctx.request.body
+      ctx.body = await personService.add({name, lastName, relation})
       ctx.body = "success"
     } catch (e) {
       console.log(e)
@@ -39,26 +39,26 @@ person.post('/',
 person.post('/:id',
   {validate: {type: CONTENT_TYPE.JSON, body: personUpdateBodyValidate}},
   koaPassport.authenticate('jwt', {session: false}),
-  (ctx) => {
-    // const { id } = ctx.params
-    // ctx.body = await personService.update(id, ctx.request.body)
+  async (ctx) => {
+    const { id } = ctx.params
+    ctx.body = await personService.update(id, ctx.request.body)
     ctx.body = "success"
   })
 
-person.delete('/:id', koaPassport.authenticate('jwt', {session: false}), (ctx) => {
-  // const result: QueryResult = await personService.delete(ctx.params.id);
-  // ctx.assert(result.summary.counters.updates().nodesDeleted !== 0, 404, 'Profile not found')
+person.delete('/:id', koaPassport.authenticate('jwt', {session: false}), async (ctx) => {
+  const result: QueryResult = await personService.delete(ctx.params.id);
+  ctx.assert(result.summary.counters.updates().nodesDeleted !== 0, 404, 'Profile not found')
   ctx.body = 'success'
 })
 person.delete(
   '/:id/relation',
   {validate: {type: CONTENT_TYPE.JSON, body: deleteRelationBodyValidate}},
   koaPassport.authenticate('jwt', {session: false}),
-  (ctx) => {
-    // const { id }: { id: string } = ctx.params;
-    // const { nodeId, direction, nodeLabel, relLabel }:DeleteRelationBody = ctx.request.body;
-    // const result = await personService.deleteRelation(id, {id: nodeId, nodeLabel, relLabel, direction});
-    // ctx.assert(result.summary.counters.updates().nodesDeleted !== 0, 404, 'Profile not found')
+  async (ctx) => {
+    const { id }: { id: string } = ctx.params;
+    const { nodeId, direction, nodeLabel, relLabel }: DeleteRelationBody = ctx.request.body;
+    const result = await personService.deleteRelation(id, {id: nodeId, nodeLabel, relLabel, direction});
+    ctx.assert(result.summary.counters.updates().nodesDeleted !== 0, 404, 'Profile not found')
     ctx.body = 'success'
   })
 export default person;
--- src/service/Person.ts
+++ src/service/Person.ts
@@ -1,7 +1,7 @@
 import { v4 } from "uuid"
 import { injectable } from "tsyringe";
 import { LABEL, QueryParams, Relation } from "./interfase";
-// import Neo4jDriver from "../neo4jDriver";
+import Neo4jDriver from "../neo4jDriver";
 import { QueryResult } from "neo4j-driver";
 import { deleteRelationByNodesIdQuery, getRelationByNodesIdQuery, updateNodeByIdQuery } from "../helpers/cyferQueryHelper";
 import { RELATION_DIRECTION } from "../constants/constants";
@@ -15,91 +15,91 @@ interface PersonData {
 @injectable()
 export default class Person {
 
-  // constructor(private neo4j: {session: {run: (q: any) => true}}) {}
+  constructor(private neo4j: Neo4jDriver) {}
 
-  // async fetchAll(queryParams: QueryParams = {}): Promise<boolean> {
-  //   const { limit, orderBy } = queryParams;
-  //   let query = `MATCH (n:${LABEL.PERSON}) Return n`;
-  //   if (orderBy) {
-  //     query = `${query} ORDER BY n.${orderBy}`
-  //   }
-  //   if (limit) {
-  //     query = `${query} LIMIT ${limit}`
-  //   }
-  //   return this.neo4j.session.run(query)
-  // }
-  //
-  // async fetch(id: string): Promise<PersonData | null> {
-  //   const queryResult = await this.neo4j.session.run(`MATCH( n:${LABEL.PERSON} { id: $id }) RETURN n`, {id})
-  //   if (!queryResult || !queryResult.records[0]) {
-  //     return null;
-  //   }
-  //   return queryResult.records[0].get('n').properties as PersonData;
-  // }
-  //
-  // async add(data: Partial<PersonData>): Promise<PersonData> {
-  //   const id: string = v4()
-  //   const {name, lastName = '', relation } = data;
-  //   try {
-  //     const tx = this.neo4j.session.beginTransaction();
-  //     const createQuery: QueryResult  = await tx.run(`CREATE ( n:${LABEL.PERSON} {id: $id, name: $name, lastName: $lastName } ) RETURN n`,
-  //       {id, name, lastName: lastName });
-  //     if (!createQuery.records || !createQuery.records.length) {
-  //       await tx.rollback()
-  //       throw new Error ('QueryResult records missing')
-  //     }
-  //     const newPerson = createQuery.records[0].get('n').properties as PersonData;
-  //
-  //     if (relation) {
-  //       const {id: mId, type: relType, direction, description: relDescription = ''} = relation
-  //       const relQuery = getRelationByNodesIdQuery(LABEL.PERSON, LABEL.PERSON, {
-  //         type: relType,
-  //         direction,
-  //         props: {description: relDescription}
-  //       })
-  //       const relationQuery = await tx.run(relQuery, {nId: id, mId, relDescription})
-  //       newPerson.relation = relationQuery.records[0].get('r').properties as Relation
-  //     }
-  //     await tx.commit();
-  //     return newPerson;
-  //   } catch (e) {
-  //     console.log(e)
-  //     throw e
-  //   }
-  // }
-  //
-  // async update(id: string, data: Partial<PersonData> ): Promise<PersonData> {
-  //   const {relation, ...props } = data
-  //   const query = updateNodeByIdQuery(LABEL.PERSON, props)
-  //   const tx = this.neo4j.session.beginTransaction();
-  //   try {
-  //     const queryResult: QueryResult = await tx.run(query, {id})
-  //     if (!queryResult.records || queryResult.records.length ) {
-  //       throw new Error('Data For update is missing')
-  //     }
-  //     const person = queryResult.records[0].get('n').properties as PersonData
-  //     if(relation) {
-  //       const { id: relId, type, direction, description } = relation;
-  //       const relQuery = getRelationByNodesIdQuery(LABEL.PERSON, LABEL.PERSON, {type, direction , props: {description}});
-  //       const relResult: QueryResult = await tx.run(relQuery, { nId: id, mId: relId})
-  //       person.relation = relResult.records[0].toObject() as Relation
-  //     }
-  //     await tx.commit();
-  //     return person
-  //   } catch (e) {
-  //     await tx.rollback();
-  //     throw e;
-  //   }
-  //
-  // }
-  //
-  // async delete(id: string): Promise<QueryResult> {
-  //   return this.neo4j.session.run(`MATCH (n:${LABEL.PERSON} { id: $id }) DETACH DELETE n`, {id});
-  // }
-  //
-  // async deleteRelation(id: string, relation: {id: string, nodeLabel: LABEL, relLabel: string, direction: RELATION_DIRECTION}): Promise<QueryResult> {
-  //   const { id: relNodeId, nodeLabel, direction, relLabel } = relation;
-  //   const queryString = deleteRelationByNodesIdQuery(LABEL.PERSON, nodeLabel, {direction, type: relLabel})
-  //   return this.neo4j.session.run(queryString, {id, relNodeId})
-  // }
+  async fetchAll(queryParams: QueryParams = {}): Promise<QueryResult> {
+    const { limit, orderBy } = queryParams;
+    let query = `MATCH (n:${LABEL.PERSON}) Return n`;
+    if (orderBy) {
+      query = `${query} ORDER BY n.${orderBy}`
+    }
+    if (limit) {
+      query = `${query} LIMIT ${limit}`
+    }
+    return this.neo4j.session.run(query)
+  }
+
+  async fetch(id: string): Promise<PersonData | null> {
+    const queryResult = await this.neo4j.session.run(`MATCH( n:${LABEL.PERSON} { id: $id }) RETURN n`, {id})
+    if (!queryResult || !queryResult.records[0]) {
+      return null;
+    }
+    return queryResult.records[0].get('n').properties as PersonData;
+  }
+
+  async add(data: Partial<PersonData>): Promise<PersonData> {
+    const id: string = v4()
+    const {name, lastName = '', relation } = data;
+    try {
+      const tx = this.neo4j.session.beginTransaction();
+      const createQuery: QueryResult  = await tx.run(`CREATE ( n:${LABEL.PERSON} {id: $id, name: $name, lastName: $lastName } ) RETURN n`,
+        {id, name, lastName: lastName });
+      if (!createQuery.records || !createQuery.records.length) {
+        await tx.rollback()
+        throw new Error ('QueryResult records missing')
+      }
+      const newPerson = createQuery.records[0].get('n').properties as PersonData;
+
+      if (relation) {
+        const {id: mId, type: relType, direction, description: relDescription = ''} = relation
+        const relQuery = getRelationByNodesIdQuery(LABEL.PERSON, LABEL.PERSON, {
+          type: relType,
+          direction,
+          props: {description: relDescription}
+        })
+        const relationQuery = await tx.run(relQuery, {nId: id, mId, relDescription})
+        newPerson.relation = relationQuery.records[0].get('r').properties as Relation
+      }
+      await tx.commit();
+      return newPerson;
+    } catch (e) {
+      console.log(e)
+      throw e
+    }
+  }
+
+  async update(id: string, data: Partial<PersonData> ): Promise<PersonData> {
+    const {relation, ...props } = data
+    const query = updateNodeByIdQuery(LABEL.PERSON, props)
+    const tx = this.neo4j.session.beginTransaction();
+    try {
+      const queryResult: QueryResult = await tx.run(query, {id})
+      if (!queryResult.records || queryResult.records.length ) {
+        throw new Error('Data For update is missing')
+      }
+      const person = queryResult.records[0].get('n').properties as PersonData
+      if(relation) {
+        const { id: relId, type, direction, description } = relation;
+        const relQuery = getRelationByNodesIdQuery(LABEL.PERSON, LABEL.PERSON, {type, direction , props: {description}});
+        const relResult: QueryResult = await tx.run(relQuery, { nId: id, mId: relId})
+        person.relation = relResult.records[0].toObject() as Relation
+      }
+      await tx.commit();
+      return person
+    } catch (e) {
+      await tx.rollback();
+      throw e;
+    }
+
+  }
+
+  async delete(id: string): Promise<QueryResult> {
+    return this.neo4j.session.run(`MATCH (n:${LABEL.PERSON} { id: $id }) DETACH DELETE n`, {id});
+  }
+
+  async deleteRelation(id: string, relation: {id: string, nodeLabel: LABEL, relLabel: string, direction: RELATION_DIRECTION}): Promise<QueryResult> {
+    const { id: relNodeId, nodeLabel, direction, relLabel } = relation;
+    const queryString = deleteRelationByNodesIdQuery(LABEL.PERSON, nodeLabel, {direction, type: relLabel})
+    return this.neo4j.session.run(queryString, {id, relNodeId})
+  }
 }
